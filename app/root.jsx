b80174bed902f07b3bf855efdae30cb0b85fffe0
Flavio Esposito <flavio29a@gmail.com>
2024-10-11 12:20:25 -0300

    Changed to express server
    - Added Vite middleware for hot module reloading

--- app/root.jsx
+++ app/root.jsx
@@ -17,7 +17,7 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <h1>Hello world!</h1>
+        <h1>Hola amigo!</h1>
         <Outlet />
 
         <Scripts />
